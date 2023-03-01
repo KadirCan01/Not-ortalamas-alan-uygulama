50390bd94c1464fd0fe16204c48dd051bfd740f8
Kadir Can ÇİFTÇİ <89932957+KadirCan01@users.noreply.github.com>
2023-03-01 14:25:07 +0300

    Add files via upload

---
+++ NotOrtalamasıAlanProgram.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NotOrtalamasıAlanProgram {
+
+	public static void main(String[] args) {
+		int quiz ,vize ,ffinal;
+		double ortalama;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Quiz Notunu Giriniz : ");
+		quiz=input.nextInt();
+		System.out.println("Vize Notunu Giriniz : ");
+		vize=input.nextInt();
+		System.out.println("Final Notunu Giriniz : ");
+		ffinal=input.nextInt();
+		
+		
+		ortalama = (quiz * 0.2) + (vize * 0.35) + (ffinal * 0.45);
+		System.out.println("Not Ortalamanız : "  +ortalama );
+		String sonuc = (ortalama >=50) ?"Geçtiniz" : "Kaldınız";
+		System.out.println(sonuc);
+		
+		
+		
+	}
+
+}
